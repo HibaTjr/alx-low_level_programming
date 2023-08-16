9dfa6d909451a950a28e09182ab3fa636a6c63a3
HibaTjr <tajerhiba1@gmail.com>
2023-08-16 13:05:08 -0700

    0-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,23 +1,12 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-void positive_or_negative(int i) {
-    if (i > 0) {
-        printf("%d is positive\n", i);
-    } else if (i < 0) {
-        printf("%d is negative\n", i);
-    } else {
-        printf("%d is zero\n", i);
-    }
-}
+#include <stdio.h>
 
-int largest_number(int a, int b, int c) {
-  
-}
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
-void print_remaining_days(int month, int day, int year) {
-    
-}
+#endif
 
-int convert_day(int month, int day) {
-   
-}
