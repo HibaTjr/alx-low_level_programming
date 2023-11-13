78bc72ae7a9c7365d21f5d72a29ee28a4ed4d8ca
HibaTjr <tajerhiba1@gmail.com>
2023-11-13 07:08:48 -0800

    commit

---
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Compile all .c files into object files
+gcc -Wall -Werror -Wextra -pedantic -fPIC -c *.c
+
+# Create the dynamic library liball.so
+gcc -shared -o liball.so *.o
+
+# Clean up object files
+rm *.o
+
